0d626203656517feae7f8b676bf3d4c96b397e05
jolujuan <thesameexample@outlook.es>
2023-02-22 21:38:05 +0100

    actualizacion desde eclipse

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -7,7 +7,7 @@ public class Peliculas {
 
 		System.out.println("Hola soy isma");
 		System.out.println("Hola soy Jose Luis :)");
-		System.out.println("Cambio segundo jossessee");
+		System.out.println("Cambio segundo jossesssee");
 		System.out.println("Hola soy ismael");
 		System.out.println("Hola soy Edu");
 		System.out.println("Segundo cambio de edusiuu");
