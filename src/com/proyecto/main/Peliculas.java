6849b2f2d5ca2a1841c8d4f3e6ed2350ca3bbadc
Ismael B B <ismbou9@alu.edu.gva.es>
2023-02-22 20:36:47 +0100

    Probando desde eclipse

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -2,7 +2,7 @@ package com.proyecto.main;
 
 public class Peliculas {
 	public static void main(String[] args) {
-		System.out.println("Hola soy isma");
+		System.out.println("Hola soy ismaelxdddd");
 	}
 
 }
