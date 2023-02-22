574b6539beb0235ab50fe6507b86944370cf9d23
Ismael B B <ismbou9@alu.edu.gva.es>
2023-02-22 21:24:54 +0100

    ya me abre en eclipse, pero sigue sin ir ese push

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -3,14 +3,7 @@ package com.proyecto.main;
 public class Peliculas {
 	public static void main(String[] args) {
 
-		System.out.println("Hola soy ismaelxdddd");
-
-		System.out.println("Hola soy isma");
-		System.out.println("Hola soy Jose Luis :)");
-		System.out.println("Hola soy ismael");
-		System.out.println("Hola soy Jose Luis");
-		System.out.println("Hola soy Edu");
-		System.out.println("Segundo cambio de edusiuu");
+	System.out.println("Borro todo porque puedo");
 
 	}
 
