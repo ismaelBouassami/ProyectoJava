84ef92667bcf132853dd10eeaf1c5fafc21b8540
jolujuan <thesameexample@outlook.es>
2023-02-22 22:03:25 +0100

    Agregar clase con nombre y mostrarlo

---
+++ src/com/proyecto/joseluis/MostraNom.java
@@ -0,0 +1,9 @@
+package com.proyecto.joseluis;
+
+public class MostraNom {
+	public String nomJoseLuis="Jose Luis";
+
+	public void mostrar() {
+		System.out.println("Nom: "+this.nomJoseLuis);
+	}
+}
