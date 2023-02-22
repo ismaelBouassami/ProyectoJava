24de11a36985c23d530d4e1f1820e782fd0b44bc
Edu Valls Gisbert <eduardvalls17@gmail.com>
2023-02-23 00:58:49 +0100

    Prueba 34

--- src/com/proyecto/joseluis/MostraNom.java
+++ src/com/proyecto/joseluis/MostraNom.java
@@ -1,9 +1,11 @@
 package com.proyecto.joseluis;
 
 public class MostraNom {
-	public String nomJoseLuis="Jose Luis";
+	public String nomJoseLuis = "Jose Luis";
+	public String nomEdu = "Edu";
 
 	public void mostrar() {
-		System.out.println("Nom: "+this.nomJoseLuis);
+		System.out.println("Nom: " + this.nomJoseLuis);
+		System.out.println("Nom: " + this.nomEdu);
 	}
 }
