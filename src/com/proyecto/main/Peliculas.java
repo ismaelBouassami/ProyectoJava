61b2bd8c628b4531c33563d72823eb69330c72b0
Ismael B B <ismbou9@alu.edu.gva.es>
2023-02-22 20:24:13 +0100

    Subido correcta

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -2,8 +2,9 @@ package com.proyecto.main;
 
 public class Peliculas {
 	public static void main(String[] args) {
-		System.out.println("Hola soy isma");
+		System.out.println("Hola soy ismael");
 		System.out.println("Hola soy Jose Luis");
+		System.out.println();
 	}
 
 }
