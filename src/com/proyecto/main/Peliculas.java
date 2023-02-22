031c57f90f307c7127ac71f574485723897034e9
joseluis <thesameexample@outlook.es>
2023-02-22 20:39:40 +0100

    actualización nombre 2 (prueba)

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -3,7 +3,7 @@ package com.proyecto.main;
 public class Peliculas {
 	public static void main(String[] args) {
 		System.out.println("Hola soy isma");
-		System.out.println("Hola soy Jose Luis");
+		System.out.println("Hola soy Jose Luis :)");
 	}
 
 }
