4684c7597bf0d7b42ad5490b2bb471ea1ff06cf1
Edu Valls Gisbert <eduardvalls17@gmail.com>
2023-02-23 12:40:28 +0100

    Hola

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -2,12 +2,14 @@ package com.proyecto.main;
 
 public class Peliculas {
 	public static void main(String[] args) {
+		
 		System.out.println("Hola soy isma");
 		System.out.println("Hola soy Jose Luis :)");
 		System.out.println("Hola soy ismael");
 		System.out.println("Hola soy Jose Luis");
 		System.out.println("Hola soy Edu");
 		System.out.println("Segundo cambio de edu");
+		
 	}
 
 }
