08ec5447ea602cc9c94e4def942796e735dfb61e
jolujuan <thesameexample@outlook.es>
2023-02-23 14:46:31 +0100

    update

--- src/com/proyecto/main/Peliculas.java
+++ src/com/proyecto/main/Peliculas.java
@@ -4,15 +4,13 @@ public class Peliculas {
 	public static void main(String[] args) {
 
 		System.out.println("Hola soy isma");
-		System.out.println("Hola soy Jose Luis :)");
-		System.out.println("Cambio segundo jossesssee");
 		System.out.println("Hola soy ismael");
 		System.out.println("Hola soy Edu");
-
 		System.out.println("Segundo cambio de edusiuu");
 		System.out.println("Borro todo porque puedo");
 		System.out.println("Javi sigue sin saber hacer comit pringao :(");
 		System.out.println("Que merda el git prim");
+		System.out.println("Mostreu el nom en la clasee!");
 
 	}
 
