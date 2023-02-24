21d381f87eab75a65432b1be84257d99dbb4d609
Javi <xspiderx310102@gmail.com>
2023-02-24 13:21:49 +0100

    Creada la classe mostrarNombreJavi

---
+++ src/com/proyecto/javier/MostrarNombreJavier.java
@@ -0,0 +1,11 @@
+package com.proyecto.javier;
+
+public class MostrarNombreJavier {
+		private String nombre = "Javi";
+
+		@Override
+		public String toString() {	
+			return "Nom: " + this.nombre;
+		}
+
+}
