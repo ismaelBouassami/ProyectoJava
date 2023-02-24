8c9cfa08ce5d4d1601adb93186db47d1f330f3a8
jolujuan <thesameexample@outlook.es>
2023-02-24 16:41:26 +0100

    prueba 2 rama

--- src/com/proyecto/main/Main.java
+++ src/com/proyecto/main/Main.java
@@ -12,7 +12,6 @@ public class Main {
 		// Joselu
 		MostrarNombreJoselu alumno1 = new MostrarNombreJoselu();
 		System.out.println(alumno1.toString());
-		System.out.println("prueba nueva en rama");
 		// Edu
 		MostrarNombreEdu alumno2 = new MostrarNombreEdu();
 		System.out.println(alumno2.toString());
