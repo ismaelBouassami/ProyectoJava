3abdff96d4051f71fb103193f1988d7b0745d8c4
Ismael B B <ismbou9@alu.edu.gva.es>
2023-02-24 12:32:34 +0100

    Ismae_creado

---
+++ src/com/proyecto/Ismael/MostrarNombreIsma.java
@@ -0,0 +1,16 @@
+package com.proyecto.Ismael;
+
+public class MostrarNombreIsma {
+	
+		private String nombre = "Edu";
+
+		@Override
+		public String toString() {
+			return "Nom: " + this.nombre;
+		}
+		
+		
+		
+	
+
+}
