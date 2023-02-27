890c6210c367ebcb52e0bd319a167c1612d3dc2f
jolujuan <thesameexample@outlook.es>
2023-02-27 22:25:23 +0100

    nombre carpeta hrisi correcto

---
+++ src/com/proyecto/hrisi/MostrarnombreHrisi.java
@@ -0,0 +1,11 @@
+package com.proyecto.hrisi;
+
+public class MostrarNombreHrisi {
+	
+	private String nombre1 = "Hrisi";
+
+	@Override
+	public String toString() {
+		return "Nom: " + this.nombre1;
+	}
+}
