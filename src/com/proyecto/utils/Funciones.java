005e816f419edc569c8fa83ee9f8b57115238dd6
Ismael B B <ismbou9@alu.edu.gva.es>
2023-02-28 16:45:30 +0100

    haciendo registro

--- src/com/proyecto/utils/Funciones.java
+++ src/com/proyecto/utils/Funciones.java
@@ -1,19 +1,31 @@
 package com.proyecto.utils;
 
+import java.util.Scanner;
+
 public class Funciones {
+	static Scanner leer = new Scanner(System.in);
 
 	// REGISTRO USUARIO //
-	public boolean registrarUsuario() {
-		// CODIGO...
-		return false;
+	public static void registrarUsuario() {
+		System.out.println("Introduce tu nombre de usuario");
+
+		System.out.println("Introduce los apellidos");
+
+		System.out.println("Introduce tu email");
+
+		System.out.println("Introduce tu poblacion¨: ");
+
+		System.out.println("Introduce tu fecha de nacimiento: ");
+
+		System.out.println("Introduce una contraseña: ");
+		
+		
 	}
-	
+
 	// LOGIN USUARIO //
-	
-	
+
 	// ELIMINAR USUARIO //
-	
-	
+
 	// ETC ETC //
-	
+
 }
