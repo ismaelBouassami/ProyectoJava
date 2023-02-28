13d842194415d9fab5965da93a637dba0f4e3a80
Edu Valls Gisbert <eduardvalls17@gmail.com>
2023-02-28 16:47:14 +0100

    Control de contraseña

--- src/com/proyecto/utils/ControlErrores.java
+++ src/com/proyecto/utils/ControlErrores.java
@@ -24,18 +24,44 @@ public class ControlErrores {
 				}
 			}
 		} while (!correcto);
-		
+
 		return n;
 	}
-	
+
 	// VALIDAR UN FLOAT //
-	
-	
+
 	// VALIDAR UN STRING //
-	
-	
+
+	// VALIDAR PASSWORD //
+	public static String pedirContraseña() {
+		Scanner entrada = new Scanner(System.in);
+		String password = "", passwordRep = "";
+		boolean verdad = false;
+
+		do {
+			System.out.println("Introduce la nueva contraseña:");
+			password = entrada.nextLine().trim().replace("\t", " ").replace(" ", "");
+
+			if (password.equals("")) {
+				System.out.println("La contraseña no puede estar vacia.");
+
+			} else if (password.length() < 5 || password.length() > 8) {
+				System.out.println("La contraseña debe ser minimo de 5 caracteres y maximo de 8.");
+			} else {
+				System.out.println("Repite la contraseña: ");
+				passwordRep = entrada.nextLine().trim().replace("\t", " ").replace(" ", "");
+				if (password.equals(passwordRep)) {
+					verdad = true;
+				} else {
+					System.out.println("Error: No ha introducido la misma contraseña. Vuelva a introducirla.");
+				}
+			}
+		} while (!verdad);
+
+		return password;
+	}
+
 	// VALIDAR UN FILE //
-	
-	
+
 	// ETC ETC //
 }
